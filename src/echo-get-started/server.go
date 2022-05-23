9937bb8f17dc6ce53e7571f3d7df6f7b2c5b3537
okitsu-mayu <okitsumayu@gmail.com>
2022-05-23 15:37:32 +0900

    値受け渡し

--- src/echo-get-started/server.go
+++ src/echo-get-started/server.go
@@ -1,13 +1,27 @@
 package main
 
 import (
+	"html/template"
+	"io"
 	"net/http"
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 )
 
+type Template struct {
+	templates *template.Template
+}
+
+func (t *Template) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
+	return t.templates.ExecuteTemplate(w, name, data)
+}
 func main() {
+
+	t := &Template{
+		templates: template.Must(template.ParseGlob("views/*.html")),
+	}
+
 	// インスタンスを作成
 	e := echo.New()
 
@@ -15,9 +29,25 @@ func main() {
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
 
+	e.Renderer = t
+
 	// ルートを設定
 	e.GET("/", hello) // ローカル環境の場合、http://localhost:1323/ にGETアクセスされるとhelloハンドラーを実行する
 
+	e.GET("/page2", func(c echo.Context) error {
+		data := struct {
+		}{}
+		return c.Render(http.StatusOK, "page2", data)
+	})
+	e.POST("/page3", func(c echo.Context) error {
+		mail := c.Request().PostFormValue("mail")
+		data := struct {
+			Mail string
+		}{
+			Mail: mail,
+		}
+		return c.Render(http.StatusOK, "page3", data)
+	})
 	// サーバーをポート番号1323で起動
 	e.Logger.Fatal(e.Start(":1323"))
 }
